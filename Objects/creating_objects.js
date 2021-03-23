5f50eac262d596aa9d053cc9585c48e04c50a92c
Vasvi Sood <76619050+vasvi-sood@users.noreply.github.com>
2021-03-23 16:14:15 +0530

    Update creating_objects.js

--- Objects/creating_objects.js
+++ Objects/creating_objects.js
@@ -1,7 +1,19 @@
-
 const personal_info=
 {
 	name: `Vasvi`,
+	Fullname: `Vasvi Sood`,
 	year: 2,
-  hobby: 'writing',
+    hobby: 'writing',
+    friends: ['a','k','j','k'],
+
 };
+//console.log(personal_info.name);
+// const prop=`name`;
+// console.log(personal_info);
+
+// console.log(personal_info[prop]);
+// console.log(personal_info[`Full`+prop]);
+
+const input=prompt('What do you wanna know about me?');
+alert(`So here's the secret: ${personal_info[input]}`)
+
