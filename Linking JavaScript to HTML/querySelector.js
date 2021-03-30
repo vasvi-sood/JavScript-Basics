ad707f99d2e4983a7319a50dc6d3c1bb79b304bb
Vasvi Sood <76619050+vasvi-sood@users.noreply.github.com>
2021-03-30 13:41:16 +0530

    Create querySelector.js

---
+++ Linking JavaScript to HTML/querySelector.js
@@ -0,0 +1,6 @@
+/*
+document: get html document
+query selector:function to select by class/ use # to select by id
+textContent: dispay what's the text of that class
+*/
+document.querySelector('.classname').textContent
